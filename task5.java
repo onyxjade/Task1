a7aab2ded23744a7eeaa4578aecf0db1659b59e6
onyxjade <148755965+onyxjade@users.noreply.github.com>
2023-11-06 10:52:51 +0000

    done adding output

--- task5.java
+++ task5.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class task5 {
     public static void main(String[] args) {
         try (Scanner input1 = new Scanner(System.in)) {
-            System.out.println("enter a word");
+            System.out.println("enter your age");
             StringBuilder sb1 = new StringBuilder(input1.nextLine());
             if(sb1.toString().equals(sb1.reverse().toString()))
                 System.out.println("The input string is a palindrome.");
