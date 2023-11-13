7c959c50f96bf73ebd03f9f0616e6360b2f6ec10
onyxjade <148755965+onyxjade@users.noreply.github.com>
2023-11-13 04:59:19 +0000

    done adding output

---
+++ .vscode/task6.java
@@ -0,0 +1,33 @@
+// Java Program to Find the Biggest of 3 Numbers
+
+
+
+class task6 {
+
+	// Function to find the biggest of three numbers
+	static int biggestOfThree(int x, int y, int z)
+	{
+
+		return z > (x > y ? x : y) ? z : ((x > y) ? x : y);
+	}
+
+	// Main driver function
+	public static void main(String[] args)
+	{
+
+		// Declaring variables for 3 numbers
+		int a, b, c;
+
+		// Variable holding the largest number
+		int largest;
+		a = 5;
+		b = 15;
+		c = 29;
+		// Calling the above function in main
+		largest = biggestOfThree(a, b, c);
+
+		// Printing the largest number
+		System.out.println(largest
+						+ " is the largest number.");
+	}
+}
