ebcf2677ed55f361fefecefe848634bca9cfcbf3
onyxjade <148755965+onyxjade@users.noreply.github.com>
2023-11-16 10:18:00 +0000

    done adding output

---
+++ task9.java
@@ -0,0 +1,31 @@
+import static java.lang.Math.*;
+
+public class task9 {
+
+    public static int add(int a , int b) {
+        System.out.println(Math.addExact(15 , 29));
+        return b;
+    }
+
+    public static int subtract(int a, int b) {
+        System.out.println(Math.subtractExact(15 , 29));
+        return b;
+    }
+
+    public static int multiply(int a, int b) {
+        System.out.println(Math.multiplyExact(15 , 29));
+        return b;
+    }
+
+    public static int divide(int a, int b) {
+        System.out.println(Math.floorDiv(15 , 29));
+        return b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(15 + 29);
+        System.out.println(15 - 29);
+        System.out.println(15 * 29);
+        System.out.println(15 / 29);
+    }
+}
