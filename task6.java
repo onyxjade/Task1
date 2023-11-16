e75b052130dd32bf0b1ef574a9b5370d864cce5f
onyxjade <148755965+onyxjade@users.noreply.github.com>
2023-11-16 09:31:38 +0000

    done adding output

---
+++ task6.java
@@ -0,0 +1,19 @@
+public class task6 {
+
+    public static void main(String[] args) {
+
+        double n1 = -5, n2 = 15, n3 = 29;
+
+        if(n1 >= n2) {
+            if(n1 >= n3)
+                System.out.println(n1 + " is the largest number.");
+            else
+                System.out.println(n3 + " is the largest number.");
+        } else {
+            if(n2 >= n3)
+                System.out.println(n2 + " is the largest number.");
+            else
+                System.out.println(n3 + " is the largest number.");
+        }
+    }
+}
